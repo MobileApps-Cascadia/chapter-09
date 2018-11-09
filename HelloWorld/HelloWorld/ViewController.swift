8c49a6041ad9d986138c9d9254449f64051dc23c
EmilioBDiaz <emiliobdiaz@cc1-230-405.local>
2018-11-08 17:02:35 -0800

    In-class activity, initial commit

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var lblName: UILabel!
     
+    @IBOutlet weak var txtNameTwo: UITextField!
+    @IBOutlet weak var lblNameTwo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -21,6 +24,21 @@ class ViewController: UIViewController {
         // grab the text from the text field
         // and write it into the label
         lblName.text = txtName.text
+        
+        lblNameTwo.text = txtNameTwo.text;
+        
+        if(txtName.text?.isEmpty)!{
+            lblName.text = "Hello";
+            lblNameTwo.text = "World!";
+        }
+    }
+    @IBAction func HelloWorldButton(_ sender: UIButton) {
+        lblName.text = "Hello World!";
+    }
+    
+    @IBAction func Clear(_ sender: UIButton) {
+        lblName.text = "Hello";
+        lblNameTwo.text = "";
     }
     
 }
